8b5b3968d7831d1f4773ff04777ecb11a8a4677d
kenedy02 <135742542+kenedy02@users.noreply.github.com>
2023-06-06 16:22:27 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testeGhithub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Uma mensagem de teste");
+            Console.ReadKey();
+        }
+    }
+}
